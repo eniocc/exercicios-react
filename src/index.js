c6bb5bf359dbb3a25734e43fb1b67eddaac4fee3
Ênio Viana <eniocc@gmail.com>
2020-02-17 01:41:25 -0300

    Relação entre componentes #1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react'
 import ReactDom from 'react-dom'
-
 // import PrimeiroComponente from './componentes/PrimeiroComponente'
 // import { ComponenteA as CompA, ComponenteB as CompB } from './componentes/DoisComponentes'
-
-import MultiElementos from './componentes/MultiElementos'
+// import MultiElementos from './componentes/MultiElementos'
+import FamiliaSilva from './componentes/FamiliaSilva'
 const elemento = document.getElementById('root')
 ReactDom.render(
     <div>
-        <MultiElementos/>        
+        <FamiliaSilva/>
+        {/* <MultiElementos/>*/}
         {/* <CompA />
         <CompB />*/}
-
         {/*<PrimeiroComponente valor="Bom dia" /> */}
     </div>
     , elemento)
